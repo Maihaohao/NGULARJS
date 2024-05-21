5e9fd0ecf658c9fcfb931a38d4f8e29240d83621
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-05-21 21:55:27 +0700

    ccccc
    aaaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductListComponent }
-];
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+     { path: 'footer', component: FooterComponent},
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { StarComponent } from './star/star.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './admin/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent,
-    HeaderComponent,
-    StarComponent,
-    ProductListComponent,
-    ParentComponent,
-    ChildComponent,
-    HomeComponent,
-    FooterComponent,
-    ContentComponent,
+    FooterComponent
   
    
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule // Add FormsModule here
+    FormsModule
+   // Add FormsModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
