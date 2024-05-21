8958706e1c694bb9a09ef32030782f813be4b2b2
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-05-21 22:12:33 +0700

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { StarComponent } from './star/star.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './admin/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    CategoriesComponent,
+    HeaderComponent,
+    StarComponent,
+    ProductListComponent,
+    ParentComponent,
+    ChildComponent,
+    HomeComponent,
+    FooterComponent,
+    ContentComponent,
   
    
   ],
